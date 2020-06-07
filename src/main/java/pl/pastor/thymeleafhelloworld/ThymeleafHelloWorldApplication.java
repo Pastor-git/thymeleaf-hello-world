3ea5b1133c2f5db912b1066dc9dd8db170841bda
Maciej Pastor <maciej.pastor@gmail.com>
2020-06-07 22:02:15 +0200

    BY BYKOWSKI:)

--- src/main/java/pl/pastor/thymeleafhelloworld/ThymeleafHelloWorldApplication.java
+++ src/main/java/pl/pastor/thymeleafhelloworld/ThymeleafHelloWorldApplication.java
@@ -1,4 +1,5 @@
 package pl.pastor.thymeleafhelloworld;
+//PROJEKT BYKOWSKI TUTORIAL
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
